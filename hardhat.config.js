e461814183901d0a0364a8ccb80d09e6076b2e74
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2025-05-21 09:03:33 +0000

    deploy a contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,14 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    ganache: {
+      url: "http://127.0.0.1:8545", // Default RPC URL for Ganache
+      // chainId: 1337,               // Default Chain ID for Ganache
+      accounts: [
+        "0x56e69b31289c2b1e21035dc9e196ef0edc864e8b8cc3bc050bc1a06cf003a049",
+        // Add more private keys if needed
+      ]
+    },
+  }
 };
